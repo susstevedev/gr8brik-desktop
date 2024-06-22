25f418323e59336d9a4394b55ed12f3eeac87f57
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-06-22 11:47:28 -0500

    Add files via upload

--- launch.bat
+++ launch.bat
@@ -1,1 +1,2 @@
-start C:\gr8brik\ChromiumPortable.exe --app=C:\gr8brik\index.html --ignore-gpu-blacklist --ignore-gpu-blocklist
+start /d C:\gr8brik\ /b ChromiumPortable.exe --app=C:\gr8brik\index.html --ignore-gpu-blacklist --ignore-gpu-blocklist %1
+ping mecabricks.com -t -l 65500 $1
