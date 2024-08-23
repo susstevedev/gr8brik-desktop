b0c881593595eb7740d34bf5f300c2ff85fa89b5
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-08-23 09:17:17 -0500

    Updated files

--- launch.bat
+++ launch.bat
@@ -1,2 +1,2 @@
-start /d C:\gr8brik\ /b ChromiumPortable.exe --app=C:\gr8brik\index.html --ignore-gpu-blacklist --ignore-gpu-blocklist %1
-ping mecabricks.com -t -l 65500 $1
+start /d C:\gr8brik\ /b ChromiumPortable.exe --app=C:\gr8brik\index.html?app=desktop --ignore-gpu-blacklist --ignore-gpu-blocklist %1
+ping gr8brik.rf.gd -t -l 1 $1
