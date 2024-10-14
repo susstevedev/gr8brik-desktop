a9c8534681c392e5793892563e9607ea91c7b4e3
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-10-14 15:27:43 -0500

    Updated to Alpha 1.2.2

--- launch.bat
+++ launch.bat
@@ -1,2 +1,1 @@
-start /d C:\gr8brik\ /b ChromiumPortable.exe --app=C:\gr8brik\index.html?app=desktop --ignore-gpu-blacklist --ignore-gpu-blocklist %1
-ping gr8brik.rf.gd -t -l 1 $1
+start /d C:\gr8brik\ /b ChromiumPortable.exe --app=C:\gr8brik\index.html --ignore-gpu-blacklist --ignore-gpu-blocklist --start-maximized %1
